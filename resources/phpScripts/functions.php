39e20389dc21cf5f9ab8d9cfd46437daa168f9f4
Michael Julander <1110mdj@gmail.com>
2017-05-19 16:52:31 -0600

    Update functions.php

--- resources/phpScripts/functions.php
+++ resources/phpScripts/functions.php
@@ -7,9 +7,16 @@ go for the framework
 function get_url(){
   $actualLink = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
   $pos = strpos($actualLink, "/index.php");
-  $relLink = substr($actualLink, $pos + strlen("/index.php"));
-  if($relLink !== ""){
-    $relLink = substr($relLink, 1);
+  if($pos > 0){
+    $relLink = substr($actualLink, $pos + strlen("/index.php"));
+    if($relLink !== ""){
+      $relLink = substr($relLink, 1);
+    }
+    if($relLink == ""){
+      $relLink = "home";
+    }
+  }else{
+    $relLink = "home";
   }
   return $relLink;
 }

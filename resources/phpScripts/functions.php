9f2005c0e0a9837a3907571def97500c4d2f0cdc
Michael Julander <1110mdj@gmail.com>
2017-05-20 15:31:46 -0600

    Update functions.php

--- resources/phpScripts/functions.php
+++ resources/phpScripts/functions.php
@@ -13,27 +13,21 @@ string login($username, $password, $sql_username, $sql_password, $sql_database);
 
 
 
-
-
-
-
 function get_url(){
-  $actualLink = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-  $pos = strpos($actualLink, "/index.php");
-  if($pos > 0){
-    $relLink = substr($actualLink, $pos + strlen("/index.php"));
+  $actualLink = $_SERVER[REQUEST_URI];
+  $pos = strpos($actualLink, "/");
+  if($pos >= 0){
+    $relLink = substr($actualLink, $pos);
     if($relLink !== ""){
       $relLink = substr($relLink, 1);
-      $pos = strpos($relLink, "/");
+      $pos = strrpos($relLink, "/");
       if($pos == strlen($relLink)-1){
         $relLink = substr($relLink, 0, $pos);
       }
     }
-    if($relLink == ""){
+    if($relLink == "" || $relLink == "index.php"){
       $relLink = "home";
     }
-  }else{
-    $relLink = "home";
   }
   return $relLink;
 }

ee4a745734b78b0194a656e22c397fb287abd437
Michael Julander <1110mdj@gmail.com>
2017-05-19 21:44:52 -0600

    Update functions.php

--- resources/phpScripts/functions.php
+++ resources/phpScripts/functions.php
@@ -2,7 +2,20 @@
 /*
 This is were the functions 
 go for the framework
+
+Here are the headers for the functions
+string get_url();
+string login($username, $password, $sql_username, $sql_password, $sql_database);
+
+
+
 */
+
+
+
+
+
+
 
 function get_url(){
   $actualLink = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
@@ -25,4 +38,21 @@ function get_url(){
   return $relLink;
 }
 
+function login($userName, $passWord, $sql_username, $sql_password, $sql_database){
+  $userName = trim($userName);
+  $passWord = trim($passWord);
+  $database = new mysqli("localhost", $sql_username, $sql_password, $sql_database);
+
+  $command = "SELECT * FROM accounts";
+  $output = mysqli_query($database, $command);
+  $information = $output->fetch_assoc();
+  $permisions = "none";
+
+  if($information[username] == $userName && $information[password] == $passWord){
+      $permisions = $information[privilages];
+  }
+
+  mysqli_close($database);
+  return $permisions;
+}
 ?>

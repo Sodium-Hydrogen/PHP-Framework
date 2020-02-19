aa2a0f546a88e8fc59464eb0753e08f1a3237ee4
Sodium-Hydrogen <42423149+Sodium-Hydrogen@users.noreply.github.com>
2020-02-18 21:10:21 -0700

    more line fixes

--- database_conf.php
+++ database_conf.php
@@ -7,7 +7,7 @@ $sql_database = "Database Name";
 // This is the mysql username
 $sql_user_name = "Database Username";
 // This is where the mysql password goes
-$sql_password = "Database Password"; 
+$sql_password = "Database Password";
 
 if($_SERVER['SCRIPT_NAME'] === '/database_conf.php'){
 	$_GET['error'] = '404';
